fe02a87ef762792809f2f4ca78c322103190152e
Mustaf-Beyazyuz <119809831+Mustaf-Beyazyuz@users.noreply.github.com>
2024-05-14 19:11:28 +0300

    Java Gelistirme

---
+++ Dersler/Ders28/Ders.java
@@ -0,0 +1,31 @@
+package Dersler.Ders28;
+
+import java.util.Scanner;
+
+public class Ders {
+   /** public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        for (;;)
+        {
+            System.out.println("Bir yazı giriniz");
+            String s1 = kb.nextLine();
+            System.out.println("Bir yazı giriniz");
+            String s2 = kb.nextLine();
+            if ("elma".equals(s1))
+                break;
+
+           int result = s1.compareTo(s2);
+
+           if (result<0)
+               System.out.printf("'%s' yazısı  '%s' yazısından önce gelir",s1,s2);
+           else if (result>0)
+               System.out.printf("'%s' yazısı '%s' yazısından önce gelir",s2,s1);
+           else
+               System.out.println("Yazılar aynı");
+
+
+
+        }
+    }**/
+
+}
